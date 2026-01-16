fe74a96c4fd7c5b0cbb6afa456fd48f46f5cbfb3
maria <mpramos78915400@gmail.com>
2026-01-15 21:41:33 -0400

    funciones dinamicas para el sidebar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,19 @@ import { Router } from "./router.js";
 const app= document.getElementById("app")
 const router = new Router(app)
 
-router.navigate("dashboard")
+//todo  funcion para marcar activo en el sidebar
+function setActive(page) { //dashboard
+    document.querySelectorAll(".menu-item").forEach((btn)=>{
+        btn.classList.toggle("active", btn.dataset.page===page)
+    })
+} 
 
-document.getElementById("dash-btn").addEventListener("click",()=>{
-    router.navigate("dashboard")
+//todo  funcion para navegar cuando hacemos click en el sidebar
+document.querySelectorAll(".menu-item").forEach(btn=>{
+        btn.addEventListener("click",()=>{
+            const page = btn.dataset.page //dashboard
+            router.navigate(page)
+            setActive(page)
+        })
 })
-document.getElementById("prod-btn").addEventListener("click",()=>{
-    router.navigate("about")
-})
+router.navigate("dashboard")
